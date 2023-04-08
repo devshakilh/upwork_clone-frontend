8b3dce73ba2e0fc746949367a1feac4b1f257f90
Programmer Bikash <bikash19081998dey@gmail.com>
2023-04-08 16:18:10 +0600

    second commit

---
+++ components/HomeComponents/HomeComponents.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const HomeComponents = () => {
+    return (
+        <div>
+            <h1 className="text-4xl font-bold text-center">This is home page</h1>
+        </div>
+    );
+};
+
+export default HomeComponents;
